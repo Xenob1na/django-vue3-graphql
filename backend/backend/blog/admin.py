68243cb53609fe9513e478e1aab3cce4c8062d65
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-10-13 22:13:21 +0700

    init

---
+++ backend/backend/blog/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+
+from .models import *
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+    list_display = (
+        "id",
+        "title",
+        "subtitle",
+        "slug",
+        "body",
+        "publish_date",
+        "published",
+    )
+    list_filter = (
+        "published",
+        "publish_date",
+    )
+    list_editable = (
+        "title",
+        "subtitle",
+        "slug",
+        "publish_date",
+        "published",
+    )
+    search_fields = (
+        "title",
+        "subtitle",
+        "slug",
+        "body",
+    )
+    prepopulated_fields =  {
+        "slug": (
+            "title",
+            "subtitle",
+        )
+    }
+    date_hierarchy = "publish_date"
+    save_on_top = True
